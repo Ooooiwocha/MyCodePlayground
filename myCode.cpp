c06b92cda233264e8536664a7ea9d694b53affd4
Ooooiwocha <77987608+Ooooiwocha@users.noreply.github.com>
2023-03-18 19:47:06 +0900

    Update myCode.cpp

--- myCode.cpp
+++ myCode.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 #include <vector>
-
-template<class N> std::istream& operator>>(std::istream& is, std::vector<N>& obj){
+#include <set>
+template<typename N> std::istream& operator>>(std::istream& is, std::vector<N>& obj){
     for(int i=0; i<obj.size(); i++){
         is >> obj[i];
     }
     return is;
 }
-template<class N> std::ostream& operator<<(std::ostream& os, std::vector<N>& obj){
+template<typename N> std::ostream& operator<<(std::ostream& os, const std::vector<N>& obj){
     for(int i=0; i<obj.size(); i++){
         os << obj[i] << "\n "[i<obj.size()-1];
     }
     return os;
 }
-template<class N> std::istream& operator>>(std::istream& is, std::pair<N, N>& obj){
+template<typename N> std::istream& operator>>(std::istream& is, std::pair<N, N>& obj){
     return is >> obj.first >> obj.second;
 }
-template<class N> std::ostream& operator<<(std::ostream& os, std::pair<N, N>& obj){
+template<typename N> std::ostream& operator<<(std::ostream& os, const std::pair<N, N>& obj){
     return os << obj.first << " " << obj.second << "\n";
 }
-
+template<typename N> std::ostream& operator<<(std::ostream& os, const std::set<N>& obj){
+    for(auto it=obj.begin(); it!=obj.end(); it++){
+        os << *it << " \n"[it==std::prev(obj.end())];
+    }
+    return os;
+}
 int main() {
-    int n; std::cin >> n;
-    std::vector<int> v(n);
-    std::pair<int, int> p;
+    auto v = std::vector<int>(3);
     std::cin >> v;
-    std::cout << v;
-    std::cin >> p;
-    std::cout << p;
+    auto s = std::set<int>(v.begin(), v.end());
+    std::cout << std::set<int>(v.begin(), v.end()) << std::endl; //仮引数においてconst参照とされていない場合、コンパイルエラーとなる
     return 0;
 }
