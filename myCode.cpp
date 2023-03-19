c338dc75ad40b41185686bc7de56dccfc071f6cc
Ooooiwocha <77987608+Ooooiwocha@users.noreply.github.com>
2023-03-20 06:40:00 +0900

    Update myCode.cpp

--- myCode.cpp
+++ myCode.cpp
@@ -41,7 +41,9 @@ template<typename N> class range{
         const N start, end, step;
     public:
         range(const N& start, const N& end, const N& step): start(start), end(end), step(step){
-            
+            if(this->step == 0){
+                throw std::runtime_error("illegal_step: range step must not be zero");
+            }
         }
         range(const N& i, const N& j): range(i, j, 1){
             
@@ -73,8 +75,8 @@ template<typename N> class range{
 template<typename N = int, class R = range<N>> class __vector: public std::vector<N>{
     public:
         using std::vector<N>::vector;
-        explicit __vector(std::vector<N>& v){
-            for(auto it=v.begin(); it!=v.end(); it++){
+        template<class C> __vector(C& container){
+            for(auto it=container.begin(); it!=container.end(); it++){
                 this->push.back(*it);
             }
         }
