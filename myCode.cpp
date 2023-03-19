cbf32603b725e31a460978096c5ca8e0494637a8
Ooooiwocha <77987608+Ooooiwocha@users.noreply.github.com>
2023-03-19 23:06:13 +0900

    Update myCode.cpp

--- myCode.cpp
+++ myCode.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <set>
+template<typename N> std::istream& operator>>(std::istream& is, std::pair<N, N>& obj){
+    return is >> obj.first >> obj.second;
+}
+template<typename N, typename M> std::ostream& operator<<(std::ostream& os, const std::pair<N, M>& obj){
+    return os << obj.first << " " << obj.second;
+}
+template<typename N> std::ostream& operator<<(std::ostream& os, const std::set<N>& obj){
+    auto prev_end = std::prev(obj.end());
+    for(auto it=obj.begin(); it!=obj.end(); it++){
+        os << *it;
+        if(it!=prev_end){
+            os << " ";
+        }
+    }
+    return os;
+}
 
 template<typename N> std::istream& operator>>(std::istream& is, std::vector<N>& obj){
     for(int i=0; i<obj.size(); i++){
@@ -9,27 +25,15 @@ template<typename N> std::istream& operator>>(std::istream& is, std::vector<N>& 
     return is;
 }
 template<typename N> std::ostream& operator<<(std::ostream& os, const std::vector<N>& obj){
-    for(int i=0; i<obj.size(); i++){
-        os << obj[i] << "\n "[i<obj.size()-1];
-    }
-    return os;
-}
-template<typename N> std::istream& operator>>(std::istream& is, std::pair<N, N>& obj){
-    return is >> obj.first >> obj.second;
-}
-template<typename N, typename M> std::ostream& operator<<(std::ostream& os, const std::pair<N, M>& obj){
-    return os << obj.first << " " << obj.second;
-}
-template<typename N> std::ostream& operator<<(std::ostream& os, const std::set<N>& obj){
     for(auto it=obj.begin(); it!=obj.end(); it++){
         os << *it;
-        if(it!=std::prev(obj.end())){
+        if (it!=std::prev(obj.end())){
             os << " ";
         }
     }
     return os;
 }
-template<typename N> class range{
+template<typename N = int> class range{
     private:
         const N start, end, step;
     public:
@@ -42,30 +46,46 @@ template<typename N> class range{
         range(const N& i): range(0, i, 1){
             
         }
-    std::vector<N> to_vector()const{
-        std::vector<N> ret;
-        for(int i=start; i<end; i+=step){
-            ret.push_back(i);
+        std::vector<N> to_vector()const{
+            std::vector<N> ret;
+            if(0<step){
+                for(N i=start; i<end; i+=step){
+                    ret.push_back(i);
+                }
+            } else {
+                for(N i=start; i>end; i+=step){
+                    ret.push_back(i);
+                }
+            }
+            return ret;
         }
-        return ret;
-    }
     
 };
-template<typename N, typename M> std::vector<N> operator&&(std::vector<N> v, range<M> r){
-    std::vector<N> ret;
-    for(auto &i: r.to_vector()){
-        ret.push_back(v.at(i));
-    }
-    return ret;
-}
+template<typename N = int, class R = range<N>> class __vector: public std::vector<N>{
+    public:
+        using std::vector<N>::vector;
+        N at(long long int index){
+            long long int __index = 0<=index? index: this->size() + index;
+            return this->std::vector<N>::at(__index);
+        }
+        N operator[](long long int index){
+            long long int __index = 0<=index? index: this->size() + index; 
+            return this->at(__index);
+        }
+        N operator[](size_t index){
+            return this->at(index);
+        }
+        __vector<N> operator[](R slice){
+            __vector<N> ret;
+            for(auto const &i: slice.to_vector()){
+                ret.push_back(this->at(i));
+            }
+            return ret;
+        }
+};
+
 int main() {
-    /*
-    auto v = std::vector<int>(3);
-    std::cin >> v;
-    auto s = std::set<int>(v.begin(), v.end());
-    auto r = v && range(0, 3, 2);
-    std::cout << std::set<int>(v.begin(), v.end()) << std::endl; //仮引数においてconst参照とされていない場合、コンパイルエラーとなる
-    std::cout << r << std::endl;
-    */
+    __vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::cout << vec[range(-8, 2, 1)] << std::endl;
     return 0;
 }
